04adbc4d3608961eb4c2de44a968377e51df022c
Paulo Michael <paulo.michael@gmail.com>
2024-05-07 05:18:51 -0300

    Implement color palette with four distinct colors, including black as the first color (#2, #3)

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function addTitle () {
-    let h1 = document.createElement('h1')
-    h1.id = 'title'
-    h1.innerHTML = 'Color Palette'
-    document.body.appendChild(h1)
+    let h1 = document.createElement('h1');
+    h1.id = 'title';
+    h1.innerHTML = 'Color Palette';
+    document.body.appendChild(h1);
 }
 addTitle();
+
+function addPalette () {
+    let colorPalette = document.createElement('div');
+    document.body.appendChild(colorPalette);
+    colorPalette.id = 'color-palette';
+    let colorList = ['black', 'red', 'green', 'blue'];
+    for (let color of colorList) {
+        let colorItem = document.createElement('div');
+        colorItem.className = 'color';
+        colorItem.style.backgroundColor = color;
+        colorPalette.appendChild(colorItem);
+    }
+}
+addPalette();
